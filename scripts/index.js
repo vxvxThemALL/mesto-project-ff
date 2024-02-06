bc10a3cc6c0851f60856032576c71f95ec7dd77a
Veniamin <kogai.vv@yandex.ru>
2024-02-06 21:56:51 +0200

    2nd try

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,33 @@
 const cardsContainer = document.querySelector('.places__list');
+const cardTemplate = document.getElementById('card-template');
 
-function addPlace(cardData, onDeleteCallBack) {
-  const cardTemplate = document.getElementById('card-template');
+function createCard(cardData, onDeleteCallBack) {
   const cardElement = cardTemplate.content.querySelector('.places__item').cloneNode(true);
 
   const cardTitle = cardElement.querySelector('.card__title');
   cardTitle.textContent = cardData.name;
 
   const cardPic = cardElement.querySelector('.card__image');
-  cardPic.setAttribute('src', cardData.link);
-
-  cardsContainer.appendChild(cardElement);
+  cardPic.src = cardData.link;
+  cardPic.alt = cardData.name;
 
   const deleteButton = cardElement.querySelector('.card__delete-button');
   deleteButton.addEventListener('click', () => {
   onDeleteCallBack(cardElement);
   })
+
+  return cardElement;
 }
 
 const cardRemove = (cardElement) => {
   cardElement.remove();
 }
 
+function addCard(cardElement) {
+  cardsContainer.appendChild(cardElement);
+}
+
 initialCards.forEach((cardData) => {
-  addPlace(cardData, cardRemove);
+  const card = createCard(cardData, cardRemove);
+  addCard(card);
 });

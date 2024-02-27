c384f005cd2930927a14bd2f8d42860d856f5542
Veniamin <kogai.vv@yandex.ru>
2024-02-27 22:55:19 +0200

    11

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import './styles/index.css';
-import { initialCards } from './scripts/cards';
-
+import { initialCards } from './components/cards.js';
+//CARD
 const cardsContainer = document.querySelector('.places__list');
 const cardTemplate = document.getElementById('card-template');
 
+//MODALS
+const popups = document.querySelectorAll('.popup');
+const popupCloseButtons = document.querySelectorAll('.popup__close');
+const popupEdit = document.querySelector('.popup_type_edit')
+const popupEditOpenButton = document.querySelector('.profile__edit-button')
+const popupNewCard = document.querySelector('.popup_type_new-card')
+const popupNewCardButton = document.querySelector('.profile__add-button');
+const popupImage = document.querySelector('.popup_type_image');
+const cardImage = document.querySelectorAll('.card__image');
+
+//CARD
 function createCard(cardData, onDeleteCallBack) {
   const cardElement = cardTemplate.content.querySelector('.places__item').cloneNode(true);
 
@@ -34,3 +45,26 @@ initialCards.forEach((cardData) => {
   const card = createCard(cardData, cardRemove);
   addCard(card);
 });
+//CARD END
+
+// MODALS
+const popupOpener = (anyPopup) => {
+  anyPopup.classList.add('popup_is-opened');
+}
+
+popupEditOpenButton.addEventListener('click', () => popupOpener(popupEdit));
+popupNewCardButton.addEventListener('click', () => popupOpener(popupNewCard));
+cardImage.forEach((image) => {
+  image.addEventListener('click', () => {
+    popupOpener(popupImage);
+  })
+})
+
+popupCloseButtons.forEach((closeButtons) => {
+  closeButtons.addEventListener('click', () => {
+    popups.forEach((popup) => {
+      if (popup.classList.contains('popup_is-opened'))
+      popup.classList.remove('popup_is-opened');
+    })
+  })
+})

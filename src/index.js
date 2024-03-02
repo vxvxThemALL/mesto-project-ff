69eb429b06e80f505a8f9e98ae5cc152f5d95595
Veniamin <kogai.vv@yandex.ru>
2024-03-02 23:51:14 +0200

    1-4 done

--- src/index.js
+++ src/index.js
@@ -5,14 +5,11 @@ const cardsContainer = document.querySelector('.places__list');
 const cardTemplate = document.getElementById('card-template');
 
 //MODALS
-const popups = document.querySelectorAll('.popup');
-const popupCloseButtons = document.querySelectorAll('.popup__close');
 const popupEdit = document.querySelector('.popup_type_edit')
 const popupEditOpenButton = document.querySelector('.profile__edit-button')
 const popupNewCard = document.querySelector('.popup_type_new-card')
 const popupNewCardButton = document.querySelector('.profile__add-button');
-const popupImage = document.querySelector('.popup_type_image');
-const cardImage = document.querySelectorAll('.card__image');
+const popupOverview = document.querySelector('.popup_type_image');
 
 //CARD
 function createCard(cardData, onDeleteCallBack) {
@@ -48,23 +45,45 @@ initialCards.forEach((cardData) => {
 //CARD END
 
 // MODALS
-const popupOpener = (anyPopup) => {
-  anyPopup.classList.add('popup_is-opened');
+const cardImageButton = document.querySelectorAll('.card__image');
+
+const popupHandler = (popup) => {
+  popup.classList.add('popup_is-opened');
+  const closeButton = popup.querySelector('.popup__close');
+  const keyHandler = (escKey) => {
+    if (escKey.key === 'Escape') {
+      popup.classList.remove('popup_is-opened');
+      document.removeEventListener('keydown', keyHandler)
+    }
+  }
+
+  closeButton.addEventListener('click', () => {
+      if (popup.classList.contains('popup_is-opened')){
+      popup.classList.remove('popup_is-opened');
+      }
+  })
+
+  popup.addEventListener('click', (event) => {
+    const isClickInsidePopupContent = event.target.closest('.popup__content');
+    if  (!isClickInsidePopupContent && popup.classList.contains('popup_is-opened')) {
+      popup.classList.remove('popup_is-opened');
+    }
+  })
+
+  document.addEventListener('keydown', keyHandler);
 }
 
-popupEditOpenButton.addEventListener('click', () => popupOpener(popupEdit));
-popupNewCardButton.addEventListener('click', () => popupOpener(popupNewCard));
-cardImage.forEach((image) => {
+cardImageButton.forEach((image) => {
   image.addEventListener('click', () => {
-    popupOpener(popupImage);
+    popupHandler(popupOverview);
+    const popupOverviewImage = popupOverview.querySelector('.popup__image');
+    const popupOverviewDesc = popupOverview.querySelector('.popup__caption')
+    popupOverviewImage.setAttribute('src', image.src);
+    popupOverviewImage.setAttribute('alt', image.alt);
+    popupOverviewDesc.textContent = image.alt;
   })
 })
+popupEditOpenButton.addEventListener('click', () => popupHandler(popupEdit));
+popupNewCardButton.addEventListener('click', () => popupHandler(popupNewCard));
 
-popupCloseButtons.forEach((closeButtons) => {
-  closeButtons.addEventListener('click', () => {
-    popups.forEach((popup) => {
-      if (popup.classList.contains('popup_is-opened'))
-      popup.classList.remove('popup_is-opened');
-    })
-  })
-})
+
